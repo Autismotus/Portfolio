13180f9a6df1219386aa87212c2d1bd40a8b2bfb
Isak Thorström <isak.thorstrom@relyit.se>
2023-07-24 13:48:07 +0200

    Second commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,77 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCards } from "./ProjectCards";
+import projImg1 from "../assets/img/ibeacon.png";
+import projImg2 from "../assets/img/väddö.png";
+import projImg3 from "../assets/img/relyfyller år.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+
 export const Projects = () => {
 
-    const projects = [
-        
-            {
-                title: "iBeacon.se",
-                description: "En beacon är en trådlös sändare med teknik som gör det möjligt för smartphones, surfplattor och andra enheter att utföra åtgärder när de befinner sig i närheten av en beacon.",
-                
-            },
-            {
-                title: "iBeacon.se",
-                description: "En beacon är en trådlös sändare med teknik som gör det möjligt för smartphones, surfplattor och andra enheter att utföra åtgärder när de befinner sig i närheten av en beacon.",
-                
-            },    
-    ];
-return (
+  const projects = [
+    {
+      title: "iBeacons",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Väddö vård AB",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Rely It fyller 15 år",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
     <section className="project" id="project">
-        <Container>
-                <Row>
-                    <Col>
-                    <h2>Projects</h2>
-                    <p>Lorem ipsum mf shit ass text for someone to read a long complicated and impressive and intrussive as well as something unproductive. </p>
-                    </Col>
-                </Row>
-            </Container>
+      <Container>
+        <Row>
+          <Col size={12}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCards
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
     </section>
-)
-    }
+  )
+}
